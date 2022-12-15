8f5de6c5c45eddaa3bd0b5e6fefa85869781c5aa
Cyril Houillon <houillon.cyril@gmail.com>
2022-12-15 23:42:55 +0100

    Day 1 : refacto ElvesParser into incomprehensible charabia fonctionnel

--- aoc2022/src/main/java/fr/ch/aoc2022/day01/ElvesParser.java
+++ aoc2022/src/main/java/fr/ch/aoc2022/day01/ElvesParser.java
@@ -1,27 +1,38 @@
 package fr.ch.aoc2022.day01;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 public class ElvesParser {
     public List<Elf> parse(List<String> input) {
 
-        List<Elf> elves = new ArrayList<>();
-
-        List<Long> currentSupplies = new ArrayList<>();
-        for (String line: input) {
-            if(line.isBlank()){
-                elves.add(new Elf(currentSupplies));
-                currentSupplies = new ArrayList<>();
-            } else {
-                currentSupplies.add(Long.valueOf(line));
-            }
-        }
-        if(!currentSupplies.isEmpty()) {
-            elves.add(new Elf(currentSupplies));
-        }
-        return elves;
+        return input.stream().collect(
+                        this::newListOfListOfString,
+                        this::accumuleLine,
+                        this::combinePartialLists).stream()
+                .map(lines -> lines.stream().map(Long::valueOf).collect(toList()))
+                .map(Elf::new)
+                .collect(toList());
     }
 
+    private ArrayList<List<String>> newListOfListOfString() {
+        return new ArrayList<List<String>>(Arrays.asList(new ArrayList<>()));
+    }
+
+    private void accumuleLine(List<List<String>> list, String line) {
+        if (line.isBlank()) {
+            list.add(new ArrayList<>());
+        } else {
+            list.get(list.size() - 1).add(line);
+        }
+    }
+
+    private void combinePartialLists(List<List<String>> list1, List<List<String>> list2) {
+        list1.get(list1.size() - 1).addAll(list2.remove(0));
+        list1.addAll(list2);
+    }
 
 }
